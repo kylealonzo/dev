f87e8a08685449baef4937b9473667fa790014d2
TEAM 404 <kylealonzo276@gmail.com>
2025-05-09 19:27:30 +0800

    ad

--- 05. impl/config.ts
+++
@@ -1,8 +0,0 @@
-export const API_CONFIG = {
-  baseURL: 'http://192.168.68.131:3000/api',
-  timeout: 10000,
-  headers: {
-    'Content-Type': 'application/json',
-    'Accept': 'application/json',
-  }
-}; 
